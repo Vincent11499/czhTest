4920c2f438a6e9b702e2c1b89b52ed207a76d612
CZH <1052335471@qq.com>
2023-01-12 08:53:33 +0800

    新增cmake 项目配置

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world"<<endl;
+    for (int i = 0; i < 8; i++)
+    {
+        cout<<"hello world"<<i<<endl;
+    }
+    system("pause");
+    return 0;
+}
+
